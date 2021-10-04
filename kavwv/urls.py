f255730d46f209e386e1d6c9ff799220e8dfc3f2
Ramesh sharma <liwinghumanz@gmail.com>
2021-10-04 19:13:26 +0530

    admin

--- kavwv/urls.py
+++ kavwv/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',include('home.urls')),
     path('registration',include('registration.urls')),
-    path('admin/', admin.site.urls),
+    path('adminsharma/', admin.site.urls),
 ]
 #urlpatterns=urlpatterns+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
